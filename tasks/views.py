c5807aafbe1f4396cc78a1031f5cd1ea9d61efa3
jimenarw <jimena.witen@gmail.com>
2025-04-25 01:33:00 -0300

    25/4

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login , logout , authenticate
@@ -47,7 +47,7 @@ def singup(request):
                 })    
 
 def tasks(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {'tasks' : tasks})
     
 def create_tasks(request):
@@ -96,7 +96,10 @@ def signin(request):
         'form': AuthenticationForm
     })
         
-        
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request,'task_detail.html',{'task': task})
    
     
 
